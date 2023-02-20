648fc8f238db49bc32f66a1eb594edda962088d5
zebbyG <zebbynjau@gmail.com>
2023-02-20 13:07:08 +0300

    Polish

--- Trial-shoe-shop/shoe-shop_trial.py
+++ Trial-shoe-shop/shoe-shop_trial.py
@@ -1,61 +1,28 @@
 print("HEEEY!!! THIS IS ZEBBYLION'S ONLINE SHOE SHOP\n")
 
-name = input("Enter your name please: ")
+while True:
+    name = input("Enter your name please: ")
+    if name:
+        break
+    else:
+        print("\033[1;31mError: Name cannot be empty. Please try again.\033[0m")
 
-print("Hello!!! " + name + "\n" + "Welcome to our shop :)\n")
+print("Hello!!!\033[33m " + name + "\033[0m\n" + "Welcome to our shop \033[32m:)\033[0m\n")
+
+brand = {
+    "1": "Jordan",
+    "2": "Nikes",
+    "3": "Yeezys",
+    "4": "Vans",
+    "5": "Pradas"
+}
+show_brands = input("\033[32mPress enter to see available brands\033[0m")
+print("We have:")
+print(f"1: {brand['1']}\n2: {brand['2']}\n3: {brand['3']}\n4: {brand['4']}\n5: {brand['5']}")
 
 
-class ShoesShop:
-    # class ShoeShop
-
-    def __init__(self, brand):
-        # initializing class ShoeShop
-        self.brand = brand
-
-    def available_brands(self):
-        # function to print the available brands
-        cont = input("press any enter to see available brands")
-        print("\nWe have:" " " + self.brand + ".")
-
-
-all_brands = ShoesShop("\n\tJordans\n\tNikes\n\tYeezys\n\tVans\n\tPradas")
-all_brands.available_brands()
-
-
-# brands = ["Jordans", "Nikes", "Yeezys", "Vans", "Pradas"]
-# cont = input("Type ~yes~ to see the brands available: ")
-# # out = False
-# for c in cont:
-#     if c == 'yes':
-#         continue
-#     # if c == 'no':
-#         # out = True
-#         # break
-# print("\nWe have the:")
-# for b in brands:
-#     # if l == "Jordans" "Nikes" "Vans" "Yeezys" "Pradas":
-#     print(b)
-#  else:
-# print("Brand not available :(\nPlease check the available brands")
-
-# print("\nThese are the brands available at our shop at the momement\n")
-
-# class Shoesshop:
-# def __init__(self, brand):
-#  self.brand = brand
-# def shoe_sale(self):
-# print("We have:" " "  + self.brand + ".")
-# availablebrands = Shoesshop("Jordans, Nikes, Yeezys, Vans and Pradas")
-# availablebrands.shoe_sale()
-# while True:
-# try:
-#       pick = str(input("Which shoe brand is your favourite:(~check spelling~):  "))
-# break
-# except NameError:
-# print("You did not enter any of the available brands")
-# except:
-# print("An unknown error occured please try again")
-Jordans = ["Air-Jordan-1", "Jordan J11 Retro Concord (2018)", "Mens Air Jordan 9 Retro", "Air Jordan 3", "Women Jordan Air 1 Low SE", "Air Jordan v"]
+Jordans = ["Air-Jordan-1", "Jordan J11 Retro Concord (2018)", "Mens Air Jordan 9 Retro", "Air Jordan 3",
+           "Women Jordan Air 1 Low SE", "Air Jordan v"]
 
 Nikes = ["Airmax 95", "Air max 270Men", "Air-Force-1", "Nike Air Huarache", "Women Nike High Top", "nike dunk low"]
 
@@ -63,84 +30,48 @@ Yeezys = ["Yeezy 700", "Yeezy 350s", "Yeezy slides", "Yeezy powerphase", "YEEZY 
 
 Vans = ["Vans old skool", "Vans Men Sk8-Hi", "Vans Slip-on", "vans Authentic", "Vans Women high cut", "Vans Sk8-mid"]
 
-Pradas = ["Prada cloudBust", "Prada Linea Rossa", "Prada cup", "Prada Punta Ala high", "Women Prada Pegasus", "Prada loafers"]
+Pradas = ["Prada cloudBust", "Prada Linea Rossa", "Prada cup", "Prada Punta Ala high", "Women Prada Pegasus",
+          "Prada loafers"]
 
-# while True:
-# try:
-pick = input("Which shoe brand is your favourite:(~check spelling~):  ")
-# for b in brands:
-#     for p in pick:
-#         if p != b:
-#             break
-#     else:
-#         continue
-#     break
-# for p in pick:
-#     if pick == Jordans or pick == Vans or pick == Pradas or pick == Yeezys or pick == Nikes:
-#         continue
-#     if pick != brands:
-#          raise NameError("Brand not available\nCheck spelling and re-enter again")
 
-# break
-# except TypeError:
-# print("You did not enter any of the available brands")
-# except:
-# print("An unknown error occured please try again")
-# for l in brands:
-# while l == "Jordans" "Nikes" "Vans" "Yeezys" "Pradas":
-# print(l)
-# else:
-# print("Brand not available :(\nPlease check the available brands")
-# break
-
-# if l == "Jordans" "Nikes" "Vans" "Yeezys" "Pradas":
-# print(l)
-# else:
-# print("Brand not available :(\nPlease check the available brands")
-# break
-# for l in brands:
-# if pick != "Prada" "Vans" "Yeezys" "Nikes" "Jordans":
-# print("Brand not available :(\nPlease check the available brands")
-# break
-# else:
-# pass
+while True:
+    try:
+        pick = int(input("Which shoe brand is your favourite:(~enter number~):  "))
+        if pick < 1 or pick > 5:
+            raise ValueError
+        break
+    except ValueError:
+        print(f"\033[1;31mRecheck and enter available brands\033[0m")
 
 for j in Jordans:
-    if pick == 'Jordans':
-        print(j)
+    if pick == 1:
+        print("\033[1;36m" + j + "\033[0m")
 
 for n in Nikes:
-    if pick == 'Nikes':
-        print(n)
+    if pick == 2:
+        print("\033[1;36m" + n + "\033[0m")
 
 for y in Yeezys:
-    if pick == 'Yeezys':
-        print(y)
+    if pick == 3:
+        print("\033[1;36m" + y + "\033[0m")
 
 for v in Vans:
-    if pick == 'Vans':
-        print(v)
+    if pick == 4:
+        print("\033[1;36m" + v + "\033[0m")
 
 for p in Pradas:
-    if pick == 'Pradas':
-        print(p)
+    if pick == 5:
+        print("\033[1;36m" + p + "\033[0m")
 
-# else:
-    # pass
-    # print("Brand not available at the moment :( \n CHECK LATER")
-# brand_picked = input("Did the brand you enterd list the shoes available in the shop:(yes/no): ")
-# for s in 'Shoeshop':
-    # if brand_picked == 'yes':
-        # continue
-    # elif brand_picked == 'no':
-        # print("Check the brand spelling and try again :)")
-    #    break
-
-
-type_shoe = input("Which shoe type in the above list would you want to buy:(~check spelling~):  ")
+while True:
+    type_shoe = input("Which shoe type in the above list would you want to buy:(~check spelling~):  ")
+    if type_shoe:
+        break
+    else:
+        print("\033[1;31m Cannot be empty. Please choose a shoe type\033[0m")
 
 if type_shoe == Jordans[0]:
-    print(Jordans[0], "retails at $25.00")
+    print(f"\033[1;39m{Jordans[0]}\033[0m retails at \033[1;34m$25.00\033[0m")
 
 elif type_shoe == Jordans[1]:
     print(Jordans[1], "retails at $21.00")
@@ -159,7 +90,6 @@ elif type_shoe == Jordans[5]:
 
 else:
     pass
-    # print("Shoe out of stock or shoe is not in the jordan brand")
 
 if type_shoe == Nikes[0]:
     print(Nikes[0], "retails at $27.50")
@@ -181,7 +111,6 @@ elif type_shoe == Nikes[5]:
 
 else:
     pass
-    # print("Shoe out of stock or shoe is not in the Nikes brand")
 
 if type_shoe == Yeezys[0]:
     print(Yeezys[0], "retails at $26.00")
@@ -203,7 +132,6 @@ elif type_shoe == Yeezys[5]:
 
 else:
     pass
-    # print("Shoe out of stock or shoe is not in the Yeezys brand")
 
 if type_shoe == Vans[0]:
     print(Vans[0], "retails at $15.00")
@@ -225,7 +153,6 @@ elif type_shoe == Vans[5]:
 
 else:
     pass
-    # print("Shoe out of stock or shoe is not in the Vans brand")
 
 if type_shoe == Pradas[0]:
     print(Pradas[0], "retails at $37.80")
@@ -247,33 +174,36 @@ elif type_shoe == Pradas[5]:
 
 else:
     pass
-    # print("Shoe out of stock or shoe is not in the Prada brand")
 
+while True:
+    report = input("\nDid you find your best fit: (yes[y]~~no[n]): ")
+    if report:
+        break
+    else:
+        print("\033[1;31m Cannot be empty\033[0m")
 
-report = input("\nDid you find your best fit: "
-               "(yes/no): ")
-# for r in report:
-#     if not r:
-#         break
-#     else:
-#         continue
-
-if report == 'yes':
-    order = input("Do you want to place an order:(yes/no): ")
-    for o in order:
-        if not o:
+if report == 'yes' or report == 'y':
+    while True:
+        order = input("Do you want to place an order:(yes[y]~~no[n]): ")
+        if order:
             break
         else:
-            continue
-    if order == 'yes':
+            print("\033[1;31m Cannot be empty\033[0m")
+    # for o in order:
+    #     if not o:
+    #         break
+    #     else:
+    #         continue
+    if order == 'yes' or order == 'y':
         import datetime
         z = datetime.datetime.now()
-        print("\033[32mYour order has been successfully placed at\033[0m", z.strftime("%Y %b %d %A %H:%M"))
+        print("\033[32mYour order has been successfully placed at\033[0m", z.strftime("%Y %b %d %A %H:%M:%S"))
         print("\nWe will contact you for more information on your order.")
     else:
         pass
 
 else:
-    print("Sorry :(\nYou can come check later to find your best fit :). Welcome!!\n")
+    print("\033[1;33mSorry :(\nYou can come check later to find your best fit :). Welcome!!\n\033[0m")
 
-print("\n\033[32mTHANKS FOR VISITING ZEBBYLION'S ONLINE SHOE SHOP :)\nHope to see you again soon\033[0m")
+print("\n\033[1;32mTHANKS FOR VISITING ZEBBYLION'S ONLINE SHOE SHOP :)\033[0m\033[1;36m\n"
+      "Hope to see you again soon\033[0m")
