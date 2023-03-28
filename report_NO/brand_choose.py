203a280beacb67a8f599d87660f6e6c6e1e01bde
zebbyG <zebbynjau@gmail.com>
2023-03-28 20:23:11 +0300

    choose brand

---
+++ report_NO/brand_choose.py
@@ -0,0 +1,16 @@
+from colorama import init, Fore, Style
+from credentials import var_global
+init()
+
+
+def choose_brand():
+    while True:
+        try:
+            var_global.pick = int(input(Fore.LIGHTMAGENTA_EX + "CHOOSE BRAND:(~enter number~)\n"
+                                        + Style.RESET_ALL))
+            print("")
+            if var_global.pick < 1 or var_global.pick > 5:
+                raise ValueError
+            break
+        except ValueError:
+            print(Fore.RED + f"Recheck and enter available brands\033[0m")
