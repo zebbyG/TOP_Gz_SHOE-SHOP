b5906ca2f4c01fb31af2e1c1808c7460019b0a4a
zebbyG <zebbynjau@gmail.com>
2023-03-28 20:23:58 +0300

    To print shoes in the brand selected

---
+++ brand_shoes/display_conditions.py
@@ -0,0 +1,19 @@
+from brand_shoes import jordans, nikes, vans, yeezys, pradas
+from credentials import var_global
+
+
+def print_shoes():
+    if var_global.pick == 1:
+        jordans.display_jordans()
+
+    elif var_global.pick == 2:
+        nikes.display_nikes()
+
+    elif var_global.pick == 3:
+        yeezys.display_yeezys()
+
+    elif var_global.pick == 4:
+        vans.display_vans()
+
+    elif var_global.pick == 5:
+        pradas.display_pradas()
