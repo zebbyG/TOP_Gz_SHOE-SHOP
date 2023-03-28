e8cae6b6099efed12f658219488fd12db60ce9bd
zebbyG <zebbynjau@gmail.com>
2023-03-28 20:20:27 +0300

    log_in to Top G'z

--- credentials/log_in.py
+++ credentials/log_in.py
@@ -1,6 +1,8 @@
+import time
+
 from colorama import init, Fore, Style
 import getpass
-from credentials import var_global
+from credentials import var_global, del_user_credentials
 init()
 
 """
@@ -12,15 +14,21 @@ def log_in():
     """"
     :Function to log in
     """
+    print(Fore.GREEN + "ACCOUNT CREATED SUCCESSFULLY" + Style.RESET_ALL)
+    time.sleep(3)
+    print(del_user_credentials.print_and_delete(Fore.YELLOW + "redirecting to log_in..."
+                                                + Style.RESET_ALL, "redirecting to log_in...", 3))
+
     print("-" * 50)
     print(Fore.BLUE + "LOG IN TO YOUR ACCOUNT" + Style.RESET_ALL)
     print("-" * 50)
     while True:
         # INVISIBLE LOG_IN PASSWORD INPUT and VISIBLE USER_NAME INPUT
         try:
-            print("Enter your username")
+            print(Fore.LIGHTMAGENTA_EX + "\nEnter your username\n" + Style.RESET_ALL)
             log_in_username = input().strip()
-            your_password = getpass.getpass(prompt="Enter your password: ", stream=None)
+            your_password = getpass.getpass(prompt=Fore.LIGHTMAGENTA_EX + "\nEnter your password:\n"
+                                            + Style.RESET_ALL, stream=None)
             print("")
             if log_in_username != var_global.new_user_name or your_password != var_global.password:
                 raise ValueError
