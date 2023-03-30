333e74914c2b09434c086d865c06e24a11101556
zebbyG <zebbynjau@gmail.com>
2023-03-30 12:48:55 +0300

    Add statement before exiting

--- credentials/exit_app.py
+++ credentials/exit_app.py
@@ -1,8 +1,13 @@
 import sys
+import time
+from colorama import init, Fore, Style
+init()
 
 
 def exit_app():
     """
     Function to exit app
     """
+    print(Fore.RED + "exiting app..." + Style.RESET_ALL)
+    time.sleep(3)
     return sys.exit()
