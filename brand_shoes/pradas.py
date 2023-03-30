578fb23c87dfb67cd6ceb997797107fb1c4cd33a
zebbyG <zebbynjau@gmail.com>
2023-03-31 01:52:28 +0300

    change values pradas

--- brand_shoes/pradas.py
+++ brand_shoes/pradas.py
@@ -9,12 +9,12 @@ def display_pradas():
     print Prada shoes and price
     """
     var_global.pradas = {
-        "Prada cloudBust": "$378",
-        "Prada Linea Rossa": "$405",
-        "Prada cup": "$231",
-        "Prada Punta Ala high": "$112",
-        "Women Prada Pegasus": "$107",
-        "Prada loafers": "$251"
+        "Prada cloudBust": "378",
+        "Prada Linea Rossa": "405",
+        "Prada cup": "231",
+        "Prada Punta Ala high": "112",
+        "Women Prada Pegasus": "107",
+        "Prada loafers": "251"
     }
 
     for p in var_global.pradas.keys():
@@ -30,22 +30,22 @@ def display_pradas():
             print(Fore.RED + "Cannot be empty. Please choose a shoe type\033[0m")
 
     if var_global.type_shoe == "Prada cloudBust":
-        print(f"{var_global.type_shoe} retails at" + Fore.BLUE + f" {var_global.pradas[var_global.type_shoe]}\033[0m")
+        print(f"{var_global.type_shoe} retails at" + Fore.BLUE + f" ${var_global.pradas[var_global.type_shoe]}\033[0m")
 
     elif var_global.type_shoe == "Prada Linea Rossa":
-        print(f"{var_global.type_shoe} retails at" + Fore.BLUE + f" {var_global.pradas[var_global.type_shoe]}\033[0m")
+        print(f"{var_global.type_shoe} retails at" + Fore.BLUE + f" ${var_global.pradas[var_global.type_shoe]}\033[0m")
 
     elif var_global.type_shoe == "Prada cup":
-        print(f"{var_global.type_shoe} retails at" + Fore.BLUE + f" {var_global.pradas[var_global.type_shoe]}\033[0m")
+        print(f"{var_global.type_shoe} retails at" + Fore.BLUE + f" ${var_global.pradas[var_global.type_shoe]}\033[0m")
 
     elif var_global.type_shoe == "Prada Punta Ala high":
-        print(f"{var_global.type_shoe} retails at" + Fore.BLUE + f" {var_global.pradas[var_global.type_shoe]}\033[0m")
+        print(f"{var_global.type_shoe} retails at" + Fore.BLUE + f" ${var_global.pradas[var_global.type_shoe]}\033[0m")
 
     elif var_global.type_shoe == "Women Prada Pegasus":
-        print(f"{var_global.type_shoe} retails at" + Fore.BLUE + f" {var_global.pradas[var_global.type_shoe]}\033[0m")
+        print(f"{var_global.type_shoe} retails at" + Fore.BLUE + f" ${var_global.pradas[var_global.type_shoe]}\033[0m")
 
     elif var_global.type_shoe == "Prada loafers":
-        print(f"{var_global.type_shoe} retails at" + Fore.BLUE + f" {var_global.pradas[var_global.type_shoe]}\033[0m")
+        print(f"{var_global.type_shoe} retails at" + Fore.BLUE + f" ${var_global.pradas[var_global.type_shoe]}\033[0m")
 
     else:
         print(Fore.RED + f"shoe {var_global.type_shoe} not found recheck and enter available shoes\n" + Style.RESET_ALL)
