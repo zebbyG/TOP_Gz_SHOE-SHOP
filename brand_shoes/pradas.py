6fb862b9a24e8af4733e84cc46a06a319267e48a
zebbyG <zebbynjau@gmail.com>
2023-03-31 01:23:46 +0300

    else pradas

--- brand_shoes/pradas.py
+++ brand_shoes/pradas.py
@@ -48,5 +48,5 @@ def display_pradas():
         print(f"{var_global.type_shoe} retails at" + Fore.BLUE + f" {var_global.pradas[var_global.type_shoe]}\033[0m")
 
     else:
-        print(Fore.RED + "shoe not found recheck and enter available shoes\n" + Style.RESET_ALL)
+        print(Fore.RED + f"shoe {var_global.type_shoe} not found recheck and enter available shoes\n" + Style.RESET_ALL)
         display_pradas()
