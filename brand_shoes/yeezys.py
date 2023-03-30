110e1874d08aa35c3c334284073e5d3592eafdb3
zebbyG <zebbynjau@gmail.com>
2023-03-31 01:52:16 +0300

    change values yeezys

--- brand_shoes/yeezys.py
+++ brand_shoes/yeezys.py
@@ -8,12 +8,12 @@ def display_yeezys():
     print yeezy shoes and price
     """
     var_global.yeezys = {
-        "Yeezy 700": "$260",
-        "Yeezy 350s": "$177",
-        "Yeezy slides": "$93",
-        "Yeezy powerphase": "$218",
-        "YEEZY 450's": "$180",
-        "Yeezy 500 Blush": "$103"
+        "Yeezy 700": "260",
+        "Yeezy 350s": "177",
+        "Yeezy slides": "93",
+        "Yeezy powerphase": "218",
+        "YEEZY 450's": "180",
+        "Yeezy 500 Blush": "103"
     }
 
     for y in var_global.yeezys:
@@ -29,22 +29,22 @@ def display_yeezys():
             print(Fore.RED + "Cannot be empty. Please choose a shoe type\033[0m")
 
     if var_global.type_shoe == "Yeezy 700":
-        print(f"{var_global.type_shoe} retails at" + Fore.BLUE + f" {var_global.yeezys[var_global.type_shoe]}\033[0m")
+        print(f"{var_global.type_shoe} retails at" + Fore.BLUE + f" ${var_global.yeezys[var_global.type_shoe]}\033[0m")
 
     elif var_global.type_shoe == "Yeezy 350s":
-        print(f"{var_global.type_shoe} retails at" + Fore.BLUE + f" {var_global.yeezys[var_global.type_shoe]}\033[0m")
+        print(f"{var_global.type_shoe} retails at" + Fore.BLUE + f" ${var_global.yeezys[var_global.type_shoe]}\033[0m")
 
     elif var_global.type_shoe == "Yeezy slides":
-        print(f"{var_global.type_shoe} retails at" + Fore.BLUE + f" {var_global.yeezys[var_global.type_shoe]}\033[0m")
+        print(f"{var_global.type_shoe} retails at" + Fore.BLUE + f" ${var_global.yeezys[var_global.type_shoe]}\033[0m")
 
     elif var_global.type_shoe == "Yeezy powerphase":
-        print(f"{var_global.type_shoe} retails at" + Fore.BLUE + f" {var_global.yeezys[var_global.type_shoe]}\033[0m")
+        print(f"{var_global.type_shoe} retails at" + Fore.BLUE + f" ${var_global.yeezys[var_global.type_shoe]}\033[0m")
 
     elif var_global.type_shoe == "YEEZY 450's":
-        print(f"{var_global.type_shoe} retails at" + Fore.BLUE + f" {var_global.yeezys[var_global.type_shoe]}\033[0m")
+        print(f"{var_global.type_shoe} retails at" + Fore.BLUE + f" ${var_global.yeezys[var_global.type_shoe]}\033[0m")
 
     elif var_global.type_shoe == "Yeezy 500 Blush":
-        print(f"{var_global.type_shoe} retails at" + Fore.BLUE + f" {var_global.yeezys[var_global.type_shoe]}\033[0m")
+        print(f"{var_global.type_shoe} retails at" + Fore.BLUE + f" ${var_global.yeezys[var_global.type_shoe]}\033[0m")
 
     else:
         print(Fore.RED + f"shoe {var_global.type_shoe} not found recheck and enter available shoes\n" + Style.RESET_ALL)
