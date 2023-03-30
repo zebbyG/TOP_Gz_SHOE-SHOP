9781ce3a6b700ee3af49c418cd60c52af160e268
zebbyG <zebbynjau@gmail.com>
2023-03-31 01:23:17 +0300

    else nikes

--- brand_shoes/nikes.py
+++ brand_shoes/nikes.py
@@ -47,5 +47,5 @@ def display_nikes():
         print(f"{var_global.type_shoe} retails at " + Fore.BLUE + f"{var_global.nikes[var_global.type_shoe]}\033[0m")
 
     else:
-        print(Fore.RED + "shoe not found recheck and enter available shoes\n" + Style.RESET_ALL)
+        print(Fore.RED + f"shoe not {var_global.type_shoe} found recheck and enter available shoes\n" + Style.RESET_ALL)
         display_nikes()
