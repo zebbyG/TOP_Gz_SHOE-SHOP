7af71f11d5b60780007e490ef1cab4939bca10a9
zebbyG <zebbynjau@gmail.com>
2023-03-30 12:48:21 +0300

    Add type_shoe[yeezys] to global

--- brand_shoes/yeezys.py
+++ brand_shoes/yeezys.py
@@ -1,5 +1,5 @@
 from colorama import init, Fore, Style
-
+from credentials import var_global
 init()
 
 
@@ -11,29 +11,29 @@ def display_yeezys():
 
     while True:
         print(Fore.LIGHTMAGENTA_EX + "CHOOSE SHOE:(~check spelling~)\n" + Style.RESET_ALL)
-        type_shoe = input()
+        var_global.type_shoe = input()
         print("")
-        if type_shoe:
+        if var_global.type_shoe:
             break
         else:
             print(Fore.RED + "Cannot be empty. Please choose a shoe type\033[0m")
 
-    if type_shoe == yeezys[0]:
+    if var_global.type_shoe == yeezys[0]:
         print(yeezys[0], "retails at" + Fore.BLUE + " $26.00\033[0m")
 
-    elif type_shoe == yeezys[1]:
+    elif var_global.type_shoe == yeezys[1]:
         print(yeezys[1], "retails at" + Fore.BLUE + " $39.00\033[0m")
 
-    elif type_shoe == yeezys[2]:
+    elif var_global.type_shoe == yeezys[2]:
         print(yeezys[2], "retails at" + Fore.BLUE + " $19.80\033[0m")
 
-    elif type_shoe == yeezys[3]:
+    elif var_global.type_shoe == yeezys[3]:
         print(yeezys[3], "retails at" + Fore.BLUE + " $21.85\033[0m")
 
-    elif type_shoe == yeezys[4]:
+    elif var_global.type_shoe == yeezys[4]:
         print(yeezys[4], "retails at" + Fore.BLUE + " $18.00\033[0m")
 
-    elif type_shoe == yeezys[5]:
+    elif var_global.type_shoe == yeezys[5]:
         print(yeezys[5], "retails at" + Fore.BLUE + " $24.90\033[0m")
 
     else:
