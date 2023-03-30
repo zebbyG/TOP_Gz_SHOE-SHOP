8af15b96ff82f73ffaa86753195814f404fd7b80
zebbyG <zebbynjau@gmail.com>
2023-03-31 00:22:18 +0300

    Transfered order option to order_shoes

--- report_NO/fit_check.py
+++ report_NO/fit_check.py
@@ -43,40 +43,8 @@ def check_fit():
         elif check_other == 2:
             exit_app.exit_app()
 
-    if var_global.report == 'yes' or var_global.report == 'y':
-        while True:
-            order = input(
-                Fore.LIGHTMAGENTA_EX + "Do you want to place an order:(yes[y]~~no[n])\n\033[0m").lower().strip()
-            if order:
-                break
-            else:
-                print(Fore.RED + "required field\033[0m")
-
-        if order == 'yes' or order == 'y':
-            order_shoes.place_order()
-        elif order == "no" or order == "n":
-
-            while True:
-                try:
-                    choice = {
-                        "1": "Back to main menu",
-                        "2": "Exit"
-                    }
-                    time.sleep(0.6)
-                    print(Fore.YELLOW + f"1: {choice['1']}\n" + Fore.RED + f"2: {choice['2']}"
-                          + Style.RESET_ALL)
-                    continue_exit = int(input().strip())
-                    if continue_exit < 1 or continue_exit > 2:
-                        raise ValueError
-                    break  # exit the loop if the input is valid
-                except ValueError:
-                    print(Fore.RED + "Invalid choice" + Style.RESET_ALL)
-
-            if continue_exit == 1:
-                brands.print_brands()
-                brand_choose.choose_brand()
-                display_conditions.print_shoes()
-                check_fit()
-                order_shoes.place_order()
-            elif continue_exit == 2:
-                exit_app.exit_app()
+    elif var_global.report == 'yes' or var_global.report == 'y':
+        order_shoes.place_order()
+    else:
+        print(Fore.RED + f"Invalid choice {var_global.report}")
+        check_fit()
